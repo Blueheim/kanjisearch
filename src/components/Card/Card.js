f7fe9ec70979cdc4ce93315e88a7cd47b3e56924
Blueheim <crystalrod0@gmail.com>
2019-01-20 17:00:12 +0100

    add deploy

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,18 @@ import classes from './Card.module.scss';
 import classNames from 'classnames';
 
 const Card = ({ id, character, meaning, kunyomi, onyomi }) => {
-  const cardClasses = classNames('box', 'm-bd-xt', 'm-rd-xt', classes.Card);
-  const headerClasses = classNames('m-tx-c');
-  const bodyClasses = classNames('side__header', 'm-fx-cl-c-c', classes.Kanji);
-  const lectureClasses = classNames('m-fx-cl m-tx-c m-bd-xt-t', classes.Lecture);
+  const cardClasses = classNames('box', 'm-bd-xt', 'm-rd-xt', 'm-mg-xt', classes.Card);
+  const headerClasses = classNames('m-tx-c', 'm-wt-700', 'm-tertiary', 'm-fs-xt');
+  const bodyClasses = classNames('side__header', 'm-fx-cl-c-c', 'm-tx-primary', classes.Kanji);
+  const lectureClasses = classNames('m-fx-cl m-tx-c m-bd-xt-t m-fs-xt', classes.Lecture);
   const spellingClasses = classNames('m-wt-900', classes.Spelling);
+  const meaningClasses = classNames(classes.Meaning);
 
-  const kunLength = kunyomi.length;
-  const onLength = onyomi.length;
+  const meaningDisplay = meaning.map((meaning, i) => (
+    <span className={meaningClasses} key={i}>
+      {meaning}
+    </span>
+  ));
 
   const kunyomiDisplay = kunyomi.map((spelling, i) => (
     <div className={spellingClasses} key={i}>
@@ -28,19 +32,19 @@ const Card = ({ id, character, meaning, kunyomi, onyomi }) => {
     <div className={cardClasses}>
       <div className="side m-fx-cl">
         <div className={headerClasses}>
-          <span>{meaning}</span>
+          <span>{meaningDisplay}</span>
         </div>
         <div className={bodyClasses}>
           <span>{character}</span>
         </div>
         <div className="side__footer m-fx-sh-c">
           <div className={lectureClasses}>
-            <div className="m-bd-xt-b">KUN</div>
-            <div className="m-fx-sh-c">{kunyomiDisplay}</div>
+            <div className="m-bd-xt-b m-primary">KUN</div>
+            <div className="m-fx-sh-c m-tx-primary">{kunyomiDisplay}</div>
           </div>
           <div className={lectureClasses}>
-            <div className="m-bd-xt-b">ON</div>
-            <div className="m-fx-sh-c">{onyomiDisplay}</div>
+            <div className="m-bd-xt-b m-secondary">ON</div>
+            <div className="m-fx-sh-c m-tx-secondary">{onyomiDisplay}</div>
           </div>
         </div>
       </div>
