24c430878dddfcd4bdcba760abeeecda6b647bcc
Blueheim <crystalrod0@gmail.com>
2019-01-20 17:10:50 +0100

    Updates

---
+++ precache-manifest.f7b374154239a0e025ef852d7f9a6a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0e9c56066e1b269aa4e",
+    "url": "/kanjisearch/static/js/runtime~main.f0e9c560.js"
+  },
+  {
+    "revision": "860181993cd4b664f668",
+    "url": "/kanjisearch/static/js/main.86018199.chunk.js"
+  },
+  {
+    "revision": "9508254f36fda41d7529",
+    "url": "/kanjisearch/static/js/1.9508254f.chunk.js"
+  },
+  {
+    "revision": "860181993cd4b664f668",
+    "url": "/kanjisearch/static/css/main.5f859aab.chunk.css"
+  },
+  {
+    "revision": "cd2cc830d4ba0033e340681e1e5912f7",
+    "url": "/kanjisearch/index.html"
+  }
+];
